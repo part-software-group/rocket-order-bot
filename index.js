7b39c6d7a986b89aba6b0374b59d90411d066e1c
pooya <pooya_azarpour@yahoo.com>
2018-09-27 13:07:17 +0330

    change 3

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const bodyParser = require('body-parser');
 const config = require('config');
+/**
+ * @type {{open, migrate, all, run}}
+ */
 const sqlite = require('sqlite');
 const Excel = require('xlsx');
 const express = require('express');
@@ -9,9 +12,19 @@ const Request = require('request-promise');
  * @property unlinkAsync
  */
 const fs = Promise.promisifyAll(require('fs'));
+/**
+ * @type {{format}}
+ */
+const persianDate = require('persian-date');
+const helper = require('./lib/helper');
 
 const port = config.get('server.http.port');
 
+/**
+ * @property use
+ * @property get
+ * @property post
+ */
 const app = express();
 app.use(bodyParser.json());
 
@@ -47,6 +60,7 @@ app.post('/hook/rocket', async (req, res) => {
     lunchListAddDate: /^\s*!lunch_list_add_date\s+([0-9]{4}-[0-9]{2}-[0-9]{2}|[0-9]{4}.[0-9]{2}.[0-9]{2}|[0-9]{4}\/[0-9]{2}\/[0-9]{2}|[0-9]{8}|[0-9]{2})\s(.+)/,
     lunchListDeleteDate: /^\s*!lunch_list_delete_date\s+([0-9]{4}-[0-9]{2}-[0-9]{2}|[0-9]{4}.[0-9]{2}.[0-9]{2}|[0-9]{4}\/[0-9]{2}\/[0-9]{2}|[0-9]{8}|[0-9]{2}|all)/,
     lunchTomorrow: /^\s*!lunch_tomorrow\s(no|pick)\s([a-z0-9-]+)\s([0-9]+)\s(.+)/,
+    lunchTomorrowReset: /^\s*!lunch_tomorrow_reset\s([a-z0-9-]+)/,
   };
 
   let selectDate;
@@ -56,6 +70,7 @@ app.post('/hook/rocket', async (req, res) => {
   const lunchListAddDate = regex.lunchListAddDate.exec(message);
   const lunchListDeleteDate = regex.lunchListDeleteDate.exec(message);
   const lunchTomorrow = regex.lunchTomorrow.exec(message);
+  const lunchTomorrowReset = regex.lunchTomorrowReset.exec(message);
 
   switch (true) {
     case Boolean(lunchListGet):
@@ -80,6 +95,9 @@ app.post('/hook/rocket', async (req, res) => {
     case Boolean(lunchTomorrow):
       updateLunchTomorrow(lunchTomorrow[1], lunchTomorrow[2], lunchTomorrow[3], lunchTomorrow[4], req.body.user_name);
       break;
+    case Boolean(lunchTomorrowReset):
+      resetLunchTomorrow(lunchTomorrowReset[1], req.body.user_name);
+      break;
   }
 
   res.setHeader('Content-Type', 'application/json');
@@ -91,7 +109,7 @@ function downloadExcel(fileId, fileName) {
     method: 'get',
     url: `${config.get('custom.rocket.url')}/file-upload/${fileId}/${fileName}`,
     headers: {
-      Cookie: 'rc_uid=y79MateTAAr3LEz6E; rc_token=iz7eRC-_zr6lGxFryjfRdyb9HBvOt44cSMwaD_5GB7P',
+      Cookie: 'rc_uid=hXX753szzaEcWzc5k; rc_token=BGT9z3k9wSnAuiHF3ZBnkHF-rXWoxDgL0ldP51N14Id',
     },
   });
 }
@@ -131,14 +149,14 @@ function downloadExcelLunch(username, fileId, fileName) {
         const [selectDate, ...selectList] = result[i];
         data.param.push(selectDate.replace(/[^0-9]+/, ''));
         data.param.push(selectList.join('|'));
-        data.param.push(getDate());
+        data.param.push(helper.getDate());
       }
 
       if (!data.text.length)
         return Promise.all([sendRocketWarning('excel_lunch_list_add_date', username), fs.unlinkAsync(file)]);
 
       Promise.all([
-        sqlite.run(`UPDATE lunch_list SET delete_date = ? WHERE delete_date = 0`, [getDate()]),
+        sqlite.run(`UPDATE lunch_list SET delete_date = ? WHERE delete_date = 0`, [helper.getDate()]),
         sqlite.run(
           `INSERT INTO lunch_list (id, order_date, list, insert_date) VALUES ${data.text.join(', ')}`,
           data.param,
@@ -194,14 +212,14 @@ function downloadExcelUser(username, fileId, fileName) {
 
         data.text.push(`(null, ?, ?)`);
         data.param.push(result[i][0]);
-        data.param.push(getDate());
+        data.param.push(helper.getDate());
       }
 
       if (!data.text.length)
         return Promise.all([sendRocketWarning('excel_person_list_add', username), fs.unlinkAsync(file)]);
 
       Promise.all([
-        sqlite.run(`UPDATE person SET delete_date = ? WHERE delete_date = 0`, [getDate()]),
+        sqlite.run(`UPDATE person SET delete_date = ? WHERE delete_date = 0`, [helper.getDate()]),
         sqlite.run(`INSERT INTO person (id, username, insert_date) VALUES ${data.text.join(', ')}`, data.param),
       ])
         .then(() => fs.unlinkAsync(file))
@@ -262,7 +280,7 @@ function addLunchListDate(selectDate, selectList, username) {
     .run(`INSERT INTO lunch_list (id, order_date, list, insert_date) VALUES (null, ?, ?, ?)`, [
       selectDate,
       selectList,
-      getDate(),
+      helper.getDate(),
     ])
     .then(() => sendRocketSuccess('lunch_list_add_date', username))
     .catch((error) =>
@@ -282,7 +300,7 @@ function addLunchListDate(selectDate, selectList, username) {
 function deleteLunchListDate(selectDate, username) {
   sqlite
     .run(`UPDATE lunch_list SET delete_date = ? WHERE order_date = ? AND delete_date = 0`, [
-      getDate(),
+      helper.getDate(),
       Number(selectDate),
     ])
     .then(() => sendRocketSuccess('lunch_list_delete_date', username))
@@ -303,19 +321,29 @@ function deleteLunchListDate(selectDate, username) {
 function updateLunchTomorrow(type, oid, listRowId, lunch, username) {
   let rocketMessageId;
   let rocketRoomId;
-  sqlite
-    .all(`SELECT lunch_list_id, rocket_message_id, rocket_room_id FROM lunch_order WHERE id = ?`, [oid])
-    .then((data) => {
+
+  Promise.all([
+    sqlite.all(`SELECT list FROM lunch_list WHERE id = ? AND delete_date = 0`, [listRowId]),
+    sqlite.all(
+      `SELECT lunch_list_id, rocket_message_id, rocket_room_id FROM lunch_order WHERE id = ? AND delete_date = 0`,
+      [oid],
+    ),
+  ])
+    .then(([lunchList, lunchOrder]) => {
+      if (!lunchList.length) return sendRocketFail('lunch_tomorrow', username);
+
+      if (lunchList[0].list.split(/\|/g).indexOf(lunch) === -1) return sendRocketFail('lunch_tomorrow_list', username);
+
       /**
        * @property lunch_list_id
        */
-      if (!data.length) return sendRocketFail('lunch_tomorrow', username);
-      else if (data.length && data[0].lunch_list_id) return sendRocketWarning('lunch_tomorrow', username);
+      if (!lunchOrder.length) return sendRocketFail('lunch_tomorrow', username);
+      else if (lunchOrder.length && lunchOrder[0].lunch_list_id) return sendRocketWarning('lunch_tomorrow', username);
 
       // noinspection JSUnresolvedVariable
-      rocketMessageId = data[0].rocket_message_id;
+      rocketMessageId = lunchOrder[0].rocket_message_id;
       // noinspection JSUnresolvedVariable
-      rocketRoomId = data[0].rocket_room_id;
+      rocketRoomId = lunchOrder[0].rocket_room_id;
       return sqlite.run(`UPDATE lunch_order SET lunch_list_id = ?, lunch = ? WHERE id = ? AND lunch_list_id ISNULL`, [
         listRowId,
         lunch,
@@ -325,13 +353,14 @@ function updateLunchTomorrow(type, oid, listRowId, lunch, username) {
     .then((data) => {
       if (!data) return;
 
-      return Request({
+      // eslint-disable-next-line
+      Request({
         method: 'post',
         url: `${config.get('custom.rocket.url')}${config.get('custom.rocket.api')}/chat.delete`,
         headers: {
           'Content-Type': 'application/json',
-          'X-User-Id': 'y79MateTAAr3LEz6E',
-          'X-Auth-Token': 'iz7eRC-_zr6lGxFryjfRdyb9HBvOt44cSMwaD_5GB7P',
+          'X-User-Id': 'hXX753szzaEcWzc5k',
+          'X-Auth-Token': 'BGT9z3k9wSnAuiHF3ZBnkHF-rXWoxDgL0ldP51N14Id',
         },
         body: {
           roomId: rocketRoomId,
@@ -339,9 +368,11 @@ function updateLunchTomorrow(type, oid, listRowId, lunch, username) {
           asUser: true,
         },
         json: true,
-      });
+      }).catch((error) => console.error(error.message.toString()));
+
+      return true;
     })
-    .then((data) => (data ? sendRocketSuccess('lunch_tomorrow', username) : null))
+    .then((data) => (data ? sendRocketSuccess('lunch_tomorrow', username, [lunch]) : null))
     .catch((error) =>
       sendRocketFail('error', username, [
         {
@@ -356,33 +387,62 @@ function updateLunchTomorrow(type, oid, listRowId, lunch, username) {
     );
 }
 
-function getDate() {
-  const datePick = new Date();
-  const year = datePick.getFullYear();
-  const month = (datePick.getMonth() + 1).toString().padStart(2, '0');
-  const day = datePick
-    .getDate()
-    .toString()
-    .padStart(2, '0');
-  const hours = datePick
-    .getHours()
-    .toString()
-    .padStart(2, '0');
-  const minutes = datePick
-    .getMinutes()
-    .toString()
-    .padStart(2, '0');
-  const seconds = datePick
-    .getSeconds()
-    .toString()
-    .padStart(2, '0');
-  const milliseconds = datePick
-    .getMilliseconds()
-    .toString()
-    .padEnd(3, '0');
-  return `${year}${month}${day}${hours}${minutes}${seconds}${milliseconds}`;
+function resetLunchTomorrow(oid, username) {
+  let start = false;
+  const info = {};
+
+  sqlite
+    .all(
+      `SELECT o.person_id, p.username, o.lunch_list_id, l.list FROM lunch_order o, person p, lunch_list l WHERE o.person_id = p.id AND o.lunch_list_id = l.id AND o.id = ? AND o.delete_date = 0`,
+      [oid],
+    )
+    .then((data) => {
+      if (!data.length) return sendRocketFail('lunch_tomorrow', username);
+
+      /**
+       * @property person_id
+       * @type {{id: *, username: string}}
+       */
+      info.person = { id: data[0].person_id, username: data[0].username };
+      info.list = [];
+      info.list.push({ id: data[0].lunch_list_id, list: data[0].list });
+      start = true;
+
+      return sqlite.run('BEGIN');
+    })
+    .then(
+      () => (start ? sqlite.run(`UPDATE lunch_order SET delete_date = ? WHERE id = ?`, [helper.getDate(), oid]) : null),
+    )
+    .then(() => (start ? helper.sendLunchRequest(sqlite, info.person, info.list, []) : null))
+    .then(() => (start ? sqlite.run('COMMIT') : null))
+    .catch((error) => {
+      if (!start) return;
+
+      return Promise.all([
+        sqlite.run('ROLLBACK'),
+        sendRocketFail('error', username, [
+          {
+            key: 'code',
+            value: 'reset_lunch_tomorrow',
+          },
+          {
+            key: 'database',
+            value: error.message.toString(),
+          },
+        ]),
+      ]);
+    })
+    .catch((error) => console.error(error));
 }
 
+/**
+ *
+ * @param command
+ * @param user
+ * @param {Array} args
+ * @param {Number} [args[][].order_date]
+ * @return {Promise<void>}
+ */
 async function sendRocketSuccess(command, user, args) {
   const body = {};
   body.channel = user.substr(0, 1) !== '@' ? `@${user}` : user;
@@ -445,8 +505,29 @@ async function sendRocketSuccess(command, user, args) {
       body.msg = 'کاربر به درستی ثبت گردید';
       break;
     case 'lunch_tomorrow':
-      body.msg =
-        'سفارش شما برای فردا ثبت شد.\n\n*درصورتی که می‌خواهید انتخاب خود را تغییر دهید به پشتیبانی مراجعه کنید.*';
+      body.msg = 'سفارش شما برای فردا ثبت شد.\n\n';
+      body.attachments = [
+        {
+          color: 'green',
+          title: `تاییدیه سفارش`,
+          fields: [
+            {
+              title: 'غذا انتخابی',
+              value: args[0],
+              short: true,
+            },
+            {
+              title: 'تاریخ ثبت',
+              value: new persianDate().format('HH:mm YYYY-MM-DD'),
+              short: true,
+            },
+          ],
+        },
+        {
+          color: 'yellow',
+          text: 'درصورتی که می‌خواهید انتخاب خود را تغییر دهید به پشتیبانی مراجعه کنید.',
+        },
+      ];
       break;
   }
 
@@ -456,8 +537,8 @@ async function sendRocketSuccess(command, user, args) {
       url: `${config.get('custom.rocket.url')}${config.get('custom.rocket.api')}/chat.postMessage`,
       headers: {
         'Content-Type': 'application/json',
-        'X-User-Id': 'y79MateTAAr3LEz6E',
-        'X-Auth-Token': 'iz7eRC-_zr6lGxFryjfRdyb9HBvOt44cSMwaD_5GB7P',
+        'X-User-Id': 'hXX753szzaEcWzc5k',
+        'X-Auth-Token': 'BGT9z3k9wSnAuiHF3ZBnkHF-rXWoxDgL0ldP51N14Id',
       },
       body,
       json: true,
@@ -502,8 +583,8 @@ async function sendRocketWarning(command, user, args) {
       url: `${config.get('custom.rocket.url')}${config.get('custom.rocket.api')}/chat.postMessage`,
       headers: {
         'Content-Type': 'application/json',
-        'X-User-Id': 'y79MateTAAr3LEz6E',
-        'X-Auth-Token': 'iz7eRC-_zr6lGxFryjfRdyb9HBvOt44cSMwaD_5GB7P',
+        'X-User-Id': 'hXX753szzaEcWzc5k',
+        'X-Auth-Token': 'BGT9z3k9wSnAuiHF3ZBnkHF-rXWoxDgL0ldP51N14Id',
       },
       body,
       json: true,
@@ -523,6 +604,9 @@ async function sendRocketFail(command, user, args) {
       break;
     case 'lunch_tomorrow':
       body.msg = 'چنین سفارشی وجود ندارد!';
+      break;
+    case 'lunch_tomorrow_list':
+      body.msg = 'غذای انتخابی در سفارش موجود نیست!';
       break;
     case 'error':
       body.msg = 'خطا در اجرا کد!';
@@ -550,8 +634,8 @@ async function sendRocketFail(command, user, args) {
       url: `${config.get('custom.rocket.url')}${config.get('custom.rocket.api')}/chat.postMessage`,
       headers: {
         'Content-Type': 'application/json',
-        'X-User-Id': 'y79MateTAAr3LEz6E',
-        'X-Auth-Token': 'iz7eRC-_zr6lGxFryjfRdyb9HBvOt44cSMwaD_5GB7P',
+        'X-User-Id': 'hXX753szzaEcWzc5k',
+        'X-Auth-Token': 'BGT9z3k9wSnAuiHF3ZBnkHF-rXWoxDgL0ldP51N14Id',
       },
       body,
       json: true,
